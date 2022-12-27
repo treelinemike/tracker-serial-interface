0d4023f19e262c3e6b9cfe8d694438d20f94bac5
treelinemike <mike.kokko@gmail.com>
2022-12-27 10:03:32 -0500

    updates

--- src/serial_test.cpp
+++ src/serial_test.cpp
@@ -1,17 +1,73 @@
-#include "serial/serial.h"
+#include "serial/serial.h" // https://www.github.com/wjwwood/serial
 #include "mak_packet.h"
 #include <iostream>
 using namespace std;
 using namespace serial;
 
-// 
-int compose_tracker_packet(char *q, char* t, char* packet, int packet_length){
+#define SIZE_Q 4
+#define SIZE_T 3
+#define MAX_PACKET_LENGTH 255
 
+// 
+int compose_tracker_packet(float *q, size_t q_size, float* t, size_t t_size, uint8_t* packet, size_t *packet_length){
+
+    // grab max packet length from input before resetting
+    size_t max_packet_length = *packet_length;
+    *packet_length = 0;
+
+
+    for(unsigned int qidx = 0; qidx < q_size; qidx++){
+       printf("q[%d] = %8.4f\n",qidx,*(q+qidx));
+    } 
+    
+    for(unsigned int tidx = 0; tidx < t_size; tidx++){
+       printf("t[%d] = %8.4f\n",tidx,*(t+tidx));
+    } 
+    
+    
+    *packet_length = 123;
     return 0;
 }
 
 int main(void){
-	cout << "Hello World!" << __linux__ << endl;
+    
+    // ensure linux system
+    // TODO: handle Windows, darwin, etc.
+    if(!__linux__){
+        printf("Error: not running on a linux system\n");
+        return -1;
+    }
+    
+    // ensure width of float type is four bytes
+    // TODO: handle other cases
+    if(sizeof(float) != 4){
+        printf("Error: float datatype is not four bytes wide\n");
+        return -1;
+    }
+    //static_assert(sizeof(float)==4,"Float size is not four bytes!"); 
+
+    // ensure little endian encoding
+    // TODO: handle big endian case
+    static float test_float = 1.254;
+    uint8_t* p_fval = (uint8_t*) &test_float;
+    if(*p_fval != 0x12){
+        printf("Error: not a litle endian system\n");
+        return -1;
+    }
+    //printf("0x%02X%02X%02X%02X\n", *p_fval, *(p_fval+1), *(p_fval+2), *(p_fval+3));
+   
+
+
+    // build a sample packet
+    float q[SIZE_Q] = {1.2,2.3,3.4,4.5};
+    float t[SIZE_T] = {5.6,6.7,7.8};
+    uint8_t mypacket[MAX_PACKET_LENGTH] = {0x00};
+    size_t mypacket_length = MAX_PACKET_LENGTH;
+    compose_tracker_packet(q,SIZE_Q,t,SIZE_T,mypacket,&mypacket_length);
+    printf("New packet length: %ld\n",mypacket_length);
+
+
+    // list serial ports 
 	vector<PortInfo> all_ports = list_ports();
 	if (all_ports.size() > 0) {
 		cout << "Available COM ports:" << endl;
@@ -20,7 +76,6 @@ int main(void){
 		}
 		cout << endl;
 	}
-
 
 	// open serial port
 	cout << "Attempting to open /dev/ttyUSB0..." << endl;
@@ -37,14 +92,6 @@ int main(void){
     // try to write something to serial port
     mySerialPort->write("Hello, world!\r\n");
 
-    // test conversion
-    //double
-    static_assert(sizeof(float)==4,"Float size is not four bytes!"); 
-    cout << "Size of float: " << sizeof(float)<< endl;
-    static float test_float = 1.254;
-    constexpr uint8_t* p_fval = (uint8_t*) &test_float;
-    printf("0x%02X%02X%02X%02X\n", *p_fval, *(p_fval+1), *(p_fval+2), *(p_fval+3));
-    static_assert(*p_fval == 12,"Not a litle endian system!");
 
     // test some functions
     usartWriteDLEStuff(mySerialPort,'A');
